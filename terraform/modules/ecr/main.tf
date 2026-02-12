c1ce7f242ff80563e17bb8fb7b70b9695b317680
D3MY <none@none.none>
2026-02-12 19:12:36 +0200

    Added github lifecycle_policy

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -6,4 +6,23 @@ resource "aws_ecr_repository" "wordpress" {
   }
 }
 
+resource "aws_ecr_lifecycle_policy" "wordpress" {
+  repository = aws_ecr_repository.wordpress.name
 
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only 10 most recent images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
